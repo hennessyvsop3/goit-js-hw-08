c78b528ab3595c63907b24ca89da255b8b963c0b
hennessyvsop3 <dimait2021@gmail.com>
2023-07-26 17:18:22 +0300

    22

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const imgMarkup = createImgEl(galleryItems);
+
+function createImgEl(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+galleryList.insertAdjacentHTML('afterbegin', imgMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */ navText: ['&#10094', '&#10095'],
+  closeText: '&#10007',
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
