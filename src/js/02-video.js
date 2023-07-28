8231ee38bcc52045a24cbd4983b9a502cc634863
hennessyvsop3 <dimait2021@gmail.com>
2023-07-28 18:45:57 +0300

    initial

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,17 @@
 import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
+const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+player.on('timeupdate', _throttle(onTimeUpdate, 1000));
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import _throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('form');
+formRef.addEventListener('input', _throttle(updateinput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+let userData = {};
+
+function updateinput(evt) {
+  const target = evt.target;
+  const formValue = target.value;
+  const formName = target.name;
+  userData[formName] = formValue;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('All fields must be filled');
+  }
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
+  console.log(userData);
+  userData = {};
+}
 
